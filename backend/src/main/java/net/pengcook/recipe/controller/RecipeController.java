2ff76b79f530474073addc737b1e469150a1a859
hyxrxn <gpfls981220@gmail.com>
2024-07-16 10:41:40 +0900

    :sparkles: retrieve home page recipes

--- backend/src/main/java/net/pengcook/recipe/controller/RecipeController.java
+++ backend/src/main/java/net/pengcook/recipe/controller/RecipeController.java
@@ -1,21 +1,30 @@
 package net.pengcook.recipe.controller;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import net.pengcook.recipe.dto.HomeRecipeRequest;
+import net.pengcook.recipe.dto.HomeRecipeResponse;
 import net.pengcook.recipe.dto.RecipeResponse;
 import net.pengcook.recipe.service.RecipeService;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/recipe")
 public class RecipeController {
 
     private final RecipeService recipeService;
 
     @GetMapping("/{id}")
+    @GetMapping("/api/home")
+    public Page<HomeRecipeResponse> readHomeRecipes(@RequestBody HomeRecipeRequest request) {
+        return recipeService.readHomeRecipes(request);
+    }
+
+    @GetMapping("/api/recipe/{id}")
     public RecipeResponse readRecipe(@PathVariable long id) {
         return recipeService.readRecipe(id);
     }
