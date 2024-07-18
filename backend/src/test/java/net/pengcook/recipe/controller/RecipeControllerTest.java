57b4b79f4794a43010b748c195bb5892b795d713
Gyeongho Yang <chchch1213@naver.com>
2024-07-18 16:16:04 +0900

    :white_check_mark: retrieve recipe by id

---
+++ backend/src/test/java/net/pengcook/recipe/controller/RecipeControllerTest.java
@@ -0,0 +1,56 @@
+package net.pengcook.recipe.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import java.time.LocalTime;
+import java.util.List;
+import net.pengcook.recipe.dto.RecipeResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql("/data.sql")
+class RecipeControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    @DisplayName("id로 레시피를 조회한다.")
+    void readRecipe() {
+        RecipeResponse expected = new RecipeResponse(
+                "Steamed Tofu with Shrimp and Eggs",
+                "http://www.foodsafetykorea.go.kr/uploadimg/cook/10_00028_2.png",
+                "https://avatars.githubusercontent.com/u/22692687?v=4",
+                "Seyang",
+                List.of("Korean food"),
+                List.of("Soft Tofu", "Cocktail Shrimp", "Egg", "Sugar", "Unsalted Butter"),
+                List.of("Heavy Cream", "Spinach"),
+                5,
+                LocalTime.parse("00:30:00"),
+                1534,
+                36056,
+                "This is steamed Tofu with Shrimp and Eggs."
+        );
+
+        RecipeResponse actual = RestAssured.given().log().all()
+                .when().get("/api/recipe/1")
+                .then().log().all()
+                .statusCode(200)
+                .extract()
+                .as(RecipeResponse.class);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
