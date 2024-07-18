d376413339d370bcd05a67808897795bd70e755a
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-07-18 11:15:18 +0900

    ➕ add ktlint plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,4 +2,9 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0" apply false
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
