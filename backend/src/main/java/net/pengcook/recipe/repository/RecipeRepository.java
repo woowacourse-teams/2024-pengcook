bac99f0991acbd4077d595d47d9f2ca4b7a9168e
Gyeongho Yang <chchch1213@naver.com>
2024-07-22 14:36:22 +0900

    :wrench: setup same develop environment

--- backend/src/main/java/net/pengcook/recipe/repository/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package net.pengcook.recipe.repository;
-
-import net.pengcook.recipe.domain.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Long> {
-}
