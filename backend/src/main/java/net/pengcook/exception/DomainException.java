87f5b7a488563afbdd2475d6511c9d5a9f923f4d
haiseong <wjdgotjd9908@gmail.com>
2024-07-23 21:51:38 +0900

    :sparkles: add http status on Custom Exception

--- backend/src/main/java/net/pengcook/exception/DomainException.java
+++ backend/src/main/java/net/pengcook/exception/DomainException.java
@@ -1,14 +1,17 @@
 package net.pengcook.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public class DomainException extends RuntimeException {
 
+    private final HttpStatus httpStatus;
     private final String title;
 
-    public DomainException(String title, String message) {
+    public DomainException(HttpStatus httpStatus, String title, String message) {
         super(message);
+        this.httpStatus = httpStatus;
         this.title = title;
     }
 }
