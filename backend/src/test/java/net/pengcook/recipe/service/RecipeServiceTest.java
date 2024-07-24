bdd8a5110c8d2fba53a90c5bfcaf39c10de4e588
hyun <osh123chm@gmail.com>
2024-07-24 16:15:52 +0900

    :white_check_mark: add recipe steps retrieval

--- backend/src/test/java/net/pengcook/recipe/service/RecipeServiceTest.java
+++ backend/src/test/java/net/pengcook/recipe/service/RecipeServiceTest.java
@@ -2,9 +2,12 @@ package net.pengcook.recipe.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import java.util.List;
 import net.pengcook.recipe.dto.MainRecipeResponse;
+import net.pengcook.recipe.dto.RecipeStepResponse;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +30,20 @@ class RecipeServiceTest {
         List<MainRecipeResponse> mainRecipeResponses = recipeService.readRecipes(pageNumber, pageSize);
 
         assertThat(mainRecipeResponses.getFirst().recipeId()).isEqualTo(expectedFirstRecipeId);
+    }
+
+    @Test
+    @DisplayName("특정 레시피의 스텝을 sequence 순서로 조회한다.")
+    void readRecipeSteps() {
+        long recipeId = 1L;
+        List<RecipeStepResponse> expectedRecipeStepResponses = Arrays.asList(
+                new RecipeStepResponse(1L, 1, "레시피1 설명1 이미지", "레시피1 설명1", 1),
+                new RecipeStepResponse(3L, 1, "레시피1 설명2 이미지", "레시피1 설명2", 2),
+                new RecipeStepResponse(2L, 1, "레시피1 설명3 이미지", "레시피1 설명3", 3)
+        );
+
+        List<RecipeStepResponse> recipeStepResponses = recipeService.readRecipeSteps(recipeId);
+
+        assertThat(recipeStepResponses).isEqualTo(expectedRecipeStepResponses);
     }
 }
