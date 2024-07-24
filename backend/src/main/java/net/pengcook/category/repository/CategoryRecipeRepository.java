7db971a49ac47ea3664bfd8cd33a6190b12e07c5
hyxrxn <gpfls981220@gmail.com>
2024-07-24 11:31:52 +0900

    :recycle: remove unnecessary annotation

--- backend/src/main/java/net/pengcook/category/repository/CategoryRecipeRepository.java
+++ backend/src/main/java/net/pengcook/category/repository/CategoryRecipeRepository.java
@@ -2,8 +2,6 @@ package net.pengcook.category.repository;
 
 import net.pengcook.category.domain.CategoryRecipe;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CategoryRecipeRepository extends JpaRepository<CategoryRecipe, Long> {
 }
