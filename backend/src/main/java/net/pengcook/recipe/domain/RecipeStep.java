f8553a361753be4f64a729b2592dac04e1c0bf2b
hyun <osh123chm@gmail.com>
2024-07-24 16:26:14 +0900

    :recycle: add access modifier to Lombok constructor

--- backend/src/main/java/net/pengcook/recipe/domain/RecipeStep.java
+++ backend/src/main/java/net/pengcook/recipe/domain/RecipeStep.java
@@ -6,9 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 public class RecipeStep {
 
