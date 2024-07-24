47acc958f961c03492a6733792022fbdcff3b6b6
hyxrxn <gpfls981220@gmail.com>
2024-07-24 11:31:52 +0900

    :recycle: remove unnecessary annotation

--- backend/src/main/java/net/pengcook/category/repository/CategoryRepository.java
+++ backend/src/main/java/net/pengcook/category/repository/CategoryRepository.java
@@ -3,9 +3,7 @@ package net.pengcook.category.repository;
 import java.util.Optional;
 import net.pengcook.category.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     Optional<Category> findByName(String name);
