e12892a0ebf7b12f12ed56a951447fb1c44478f1
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-07-24 17:05:43 +0900

    :lipstick: add layout components for implementing sign up

--- android/app/src/main/java/net/pengcook/android/presentation/BindingAdapters.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/BindingAdapters.kt
@@ -1,8 +1,14 @@
 package net.pengcook.android.presentation
 
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
 import com.bumptech.glide.Glide
 import net.pengcook.android.R
 
@@ -55,3 +61,49 @@ fun timeRequiredText(
     val context = view.context
     view.text = context.getString(R.string.time_format_required).format(time)
 }
+
+@BindingAdapter("bind:selectedValue")
+fun bindSpinnerData(
+    spinner: Spinner,
+    newValue: String?,
+) {
+    val adapter = spinner.adapter as? ArrayAdapter<String>
+    val position = adapter?.getPosition(newValue)
+    if (position != null && position != spinner.selectedItemPosition) {
+        spinner.setSelection(position, true)
+    }
+}
+
+@InverseBindingAdapter(attribute = "bind:selectedValue", event = "bind:selectedValueAttrChanged")
+fun captureSelectedValue(spinner: Spinner): String {
+    return spinner.selectedItem as String
+}
+
+@BindingAdapter("bind:selectedValueAttrChanged")
+fun setSpinnerListener(
+    spinner: Spinner,
+    listener: InverseBindingListener?,
+) {
+    if (listener == null) {
+        spinner.onItemSelectedListener = null
+        return
+    }
+
+    val onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(
+            parent: AdapterView<*>,
+            view: View?,
+            position: Int,
+            id: Long,
+        ) {
+            listener.onChange()
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>) {
+            // do nothing
+        }
+    }
+
+    spinner.onItemSelectedListener = onItemSelectedListener
+}
+
