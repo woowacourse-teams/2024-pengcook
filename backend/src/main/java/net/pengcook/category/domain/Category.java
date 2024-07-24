7cb01fdcb9e20f4fba9c4d58b0b53a8586b1ca6f
hyxrxn <gpfls981220@gmail.com>
2024-07-24 11:11:25 +0900

    :recycle: add access modifier to Lombok constructor

--- backend/src/main/java/net/pengcook/category/domain/Category.java
+++ backend/src/main/java/net/pengcook/category/domain/Category.java
@@ -4,12 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Category {
 
     @Id
--- backend/src/main/java/net/pengcook/category/domain/CategoryRecipe.java
+++ backend/src/main/java/net/pengcook/category/domain/CategoryRecipe.java
@@ -6,13 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import net.pengcook.recipe.domain.Recipe;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CategoryRecipe {
 
     @Id
