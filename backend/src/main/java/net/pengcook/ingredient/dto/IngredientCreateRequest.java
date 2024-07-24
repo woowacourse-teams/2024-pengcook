277935662edd3d28a137a63ae5feffec51177195
tackyu <tack970913@gmail.com>
2024-07-24 23:42:37 +0900

    :recycle: use custom exception for ingredient

--- backend/src/main/java/net/pengcook/ingredient/dto/IngredientCreateRequest.java
+++ backend/src/main/java/net/pengcook/ingredient/dto/IngredientCreateRequest.java
@@ -1,8 +1,8 @@
 package net.pengcook.ingredient.dto;
 
 import java.util.List;
-
 import net.pengcook.ingredient.domain.Requirement;
+import net.pengcook.ingredient.exception.InvalidNameException;
 
 public record IngredientCreateRequest(
         String name,
@@ -19,13 +19,13 @@ public record IngredientCreateRequest(
 
     private void validateName(String name) {
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException("name cannot be null or empty");
+            throw new InvalidNameException("name cannot be null or empty");
         }
     }
 
     private void validateSubstitutions(List<String> substitutions) {
         if (substitutions == null) {
-            throw new IllegalArgumentException("substitutions cannot be null");
+            throw new InvalidNameException("substitutions cannot be null");
         }
         for (String substitution : substitutions) {
             validateName(substitution);
