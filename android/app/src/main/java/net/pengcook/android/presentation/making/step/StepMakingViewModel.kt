a30021495da855d43804f209f9296c3728659ae3
ii2001 <iivks2001@gmail.com>
2024-07-25 21:49:10 +0900

    :sparkles: add liveData

--- android/app/src/main/java/net/pengcook/android/presentation/making/step/StepMakingViewModel.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/making/step/StepMakingViewModel.kt
@@ -1,7 +1,17 @@
 package net.pengcook.android.presentation.making.step
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import net.pengcook.android.presentation.making.step.listener.StepEventListener
 
-class StepMakingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class StepMakingViewModel : ViewModel(), StepEventListener {
+    private val _stepNumber = MutableLiveData<String>()
+    val stepNumber: LiveData<String> get() = _stepNumber
+
+    val introductionContent = MutableLiveData<String>()
+
+    override fun onNavigateToNextStep() {
+        TODO("Not yet implemented")
+    }
 }
