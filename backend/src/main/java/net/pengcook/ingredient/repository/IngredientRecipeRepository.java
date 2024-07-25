0e3a81c0b15a3c4d4c4ec3c6e75c7e5e1b8f2fcb
tackyu <tack970913@gmail.com>
2024-07-25 13:35:33 +0900

    :recycle: eliminate unnecessary annotation

--- backend/src/main/java/net/pengcook/ingredient/repository/IngredientRecipeRepository.java
+++ backend/src/main/java/net/pengcook/ingredient/repository/IngredientRecipeRepository.java
@@ -1,10 +1,7 @@
 package net.pengcook.ingredient.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import net.pengcook.ingredient.domain.IngredientRecipe;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface IngredientRecipeRepository extends JpaRepository<IngredientRecipe, Long> {
 }
--- backend/src/main/java/net/pengcook/ingredient/repository/IngredientSubstitutionRepository.java
+++ backend/src/main/java/net/pengcook/ingredient/repository/IngredientSubstitutionRepository.java
@@ -1,10 +1,7 @@
 package net.pengcook.ingredient.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import net.pengcook.ingredient.domain.IngredientSubstitution;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
 public interface IngredientSubstitutionRepository extends JpaRepository<IngredientSubstitution, Long> {
 }
