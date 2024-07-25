862fea733a30d0ddd8f457f1342218b150c379eb
Hogu59 <dbstjdguswkd@naver.com>
2024-07-25 22:03:08 +0900

    :sparkles: add UIModel (RecipeStep)

--- android/app/src/main/java/net/pengcook/android/presentation/detail/RecipeStep.kt
+++
@@ -1,9 +0,0 @@
-package net.pengcook.android.presentation.detail
-
-data class RecipeStep(
-    val stepId: Long,
-    val recipeId: Int,
-    val description: String,
-    val image: Any,
-    val sequence: Int,
-)
