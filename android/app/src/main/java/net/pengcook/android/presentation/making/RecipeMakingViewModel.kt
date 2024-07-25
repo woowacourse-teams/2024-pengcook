251f3ea5729ab334125a29edeb2130ffa26f73f4
ii2001 <iivks2001@gmail.com>
2024-07-25 21:49:10 +0900

    :sparkles: add liveData

--- android/app/src/main/java/net/pengcook/android/presentation/making/RecipeMakingViewModel.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/making/RecipeMakingViewModel.kt
@@ -1,7 +1,22 @@
 package net.pengcook.android.presentation.making
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import net.pengcook.android.presentation.making.listener.RecipeMakingEventListener
 
-class RecipeMakingViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class RecipeMakingViewModel : ViewModel(), RecipeMakingEventListener {
+    val titleContent = MutableLiveData<String>()
+
+    val categorySelectedValue = MutableLiveData<String>()
+
+    val ingredientContent = MutableLiveData<String>()
+
+    val difficultySelectedValue = MutableLiveData<String>()
+
+    val introductionContent = MutableLiveData<String>()
+
+    override fun onNavigateToStep() {
+        // TODO: Implement navigation logic
+    }
 }
