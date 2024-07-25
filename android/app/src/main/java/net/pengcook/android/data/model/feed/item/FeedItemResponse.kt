6fe6c4881a1f4376a550d1e29054173923308742
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-07-25 18:28:47 +0900

    :sparkles: add DTOs for recipe responses

---
+++ android/app/src/main/java/net/pengcook/android/data/model/feed/item/FeedItemResponse.kt
@@ -0,0 +1,27 @@
+package net.pengcook.android.data.model.feed.item
+
+
+import com.google.gson.annotations.SerializedName
+
+data class FeedItemResponse(
+    @SerializedName("author")
+    val author: AuthorResponse,
+    @SerializedName("category")
+    val category: List<CategoryResponse>,
+    @SerializedName("cookingTime")
+    val cookingTime: String,
+    @SerializedName("description")
+    val description: String,
+    @SerializedName("difficulty")
+    val difficulty: Int,
+    @SerializedName("ingredient")
+    val ingredient: List<IngredientResponse>,
+    @SerializedName("likeCount")
+    val likeCount: Int,
+    @SerializedName("recipeId")
+    val recipeId: Long,
+    @SerializedName("thumbnail")
+    val thumbnail: String,
+    @SerializedName("title")
+    val title: String
+)
