f71c24af81dad0af5ae508f3e5bcb6a560096e46
ii2001 <iivks2001@gmail.com>
2024-07-25 17:51:58 +0900

    :sparkles: impl recipe making fragment

---
+++ android/app/src/main/java/net/pengcook/android/presentation/making/RecipeMakingFragment.kt
@@ -0,0 +1,39 @@
+package net.pengcook.android.presentation.making
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import net.pengcook.android.databinding.FragmentRecipeMakingBinding
+
+class RecipeMakingFragment : Fragment() {
+    private var _binding: FragmentRecipeMakingBinding? = null
+    private val binding: FragmentRecipeMakingBinding
+        get() = _binding!!
+    private val viewModel: RecipeMakingViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRecipeMakingBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initBinding()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initBinding() {
+        binding.lifecycleOwner = this
+        binding.vm = viewModel
+    }
+}
---
+++ android/app/src/main/java/net/pengcook/android/presentation/making/RecipeMakingViewModel.kt
@@ -0,0 +1,7 @@
+package net.pengcook.android.presentation.making
+
+import androidx.lifecycle.ViewModel
+
+class RecipeMakingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
