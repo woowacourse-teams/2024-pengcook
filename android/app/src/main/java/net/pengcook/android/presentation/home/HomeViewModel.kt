78091223a44729890301f7c184c581c25d076460
ii2001 <iivks2001@gmail.com>
2024-07-26 01:36:41 +0900

    :sparkles: impl navigate home to detail

--- android/app/src/main/java/net/pengcook/android/presentation/home/HomeViewModel.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/home/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package net.pengcook.android.presentation.home
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
@@ -11,11 +12,16 @@ import androidx.paging.liveData
 import net.pengcook.android.data.datasource.FeedPagingSource
 import net.pengcook.android.data.repository.feed.FeedRepository
 import net.pengcook.android.presentation.core.model.Recipe
+import net.pengcook.android.presentation.core.util.Event
 import net.pengcook.android.presentation.home.listener.FeedItemEventListener
 
 class HomeViewModel(
     private val feedRepository: FeedRepository,
 ) : ViewModel(), FeedItemEventListener {
+    private val _uiEvent: MutableLiveData<Event<HomeEvent>> = MutableLiveData()
+    val uiEvent: LiveData<Event<HomeEvent>>
+        get() = _uiEvent
+
     val feedData: LiveData<PagingData<Recipe>> =
         Pager(
             config = PagingConfig(pageSize = PAGE_SIZE, enablePlaceholders = false),
@@ -24,11 +30,15 @@ class HomeViewModel(
             .liveData
             .cachedIn(viewModelScope)
 
+    override fun onNavigateToDetail(recipe: Recipe) {
+        _uiEvent.value = Event(HomeEvent.NavigateToDetail(recipe))
+    }
+
     companion object {
         private const val PAGE_SIZE = 10
     }
+}
 
-    override fun onNavigateToDetail(recipe: Recipe) {
-        // Navigate to detail page
-    }
+sealed interface HomeEvent {
+    data class NavigateToDetail(val recipe: Recipe) : HomeEvent
 }
