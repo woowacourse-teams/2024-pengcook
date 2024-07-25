7815ccf675e643dea27c348450837d02679c42c8
haiseong <wjdgotjd9908@gmail.com>
2024-07-25 11:21:17 +0900

    :sparkles: spring MVC Exception Handling

--- backend/src/main/java/net/pengcook/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/net/pengcook/exception/GlobalExceptionHandler.java
@@ -1,15 +1,32 @@
 package net.pengcook.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        log.error(ex.getMessage(), ex);
+
+        return super.handleExceptionInternal(ex, null, headers, status, request);
+    }
 
     @ExceptionHandler(DomainException.class)
     public ResponseEntity<ProblemDetail> handleDomainException(DomainException ex) {
@@ -25,7 +42,6 @@ public class GlobalExceptionHandler {
         log.error(ex.getMessage(), ex);
 
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-        problemDetail.setTitle("EXCEPTION");
         problemDetail.setDetail("서버에서 알 수 없는 오류가 발생하였습니다.");
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(problemDetail);
     }
