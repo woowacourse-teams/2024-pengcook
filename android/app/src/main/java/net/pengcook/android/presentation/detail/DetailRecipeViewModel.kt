9882fab3de6c3830ac6b959458bec4a70ac66845
ii2001 <iivks2001@gmail.com>
2024-07-26 02:44:15 +0900

    :sparkles: impl navigate detail to cooking step

--- android/app/src/main/java/net/pengcook/android/presentation/detail/DetailRecipeViewModel.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/detail/DetailRecipeViewModel.kt
@@ -1,8 +1,16 @@
 package net.pengcook.android.presentation.detail
 
+// DetailRecipeViewModel.kt
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import net.pengcook.android.presentation.core.model.Recipe
 
-class DetailRecipeViewModel(
-    private val recipe: Recipe,
-) : ViewModel()
+class DetailRecipeViewModel(private val recipe: Recipe) : ViewModel() {
+    private val _navigateToStepEvent = MutableLiveData<Boolean>()
+    val navigateToStepEvent: LiveData<Boolean> get() = _navigateToStepEvent
+
+    fun onNavigateToMakingStep() {
+        _navigateToStepEvent.value = true
+    }
+}
