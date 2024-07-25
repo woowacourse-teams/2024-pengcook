34dd93168f2e07f7a0171caa8976c4dd7c6b7b96
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-07-25 19:02:23 +0900

    :recycle: remove redundant @SerializedName annotations

--- android/app/src/main/java/net/pengcook/android/data/model/feed/item/AuthorResponse.kt
+++ android/app/src/main/java/net/pengcook/android/data/model/feed/item/AuthorResponse.kt
@@ -1,12 +1,7 @@
 package net.pengcook.android.data.model.feed.item
 
-import com.google.gson.annotations.SerializedName
-
 data class AuthorResponse(
-    @SerializedName("authorId")
     val authorId: Long,
-    @SerializedName("authorImage")
     val authorImage: String,
-    @SerializedName("authorName")
     val authorName: String,
 )
--- android/app/src/main/java/net/pengcook/android/data/model/feed/item/CategoryResponse.kt
+++ android/app/src/main/java/net/pengcook/android/data/model/feed/item/CategoryResponse.kt
@@ -1,10 +1,6 @@
 package net.pengcook.android.data.model.feed.item
 
-import com.google.gson.annotations.SerializedName
-
 data class CategoryResponse(
-    @SerializedName("categoryId")
     val categoryId: Long,
-    @SerializedName("categoryName")
     val categoryName: String,
 )
