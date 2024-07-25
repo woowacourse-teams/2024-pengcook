1a1f67d3a62647ce7d6985f3d8690a35c2df0310
Gyeongho Yang <chchch1213@naver.com>
2024-07-25 19:19:42 +0900

    :bug: fix on using different docs library

--- backend/src/test/java/net/pengcook/category/controller/CategoryControllerTest.java
+++ backend/src/test/java/net/pengcook/category/controller/CategoryControllerTest.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.jdbc.Sql;
 
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
 import static org.hamcrest.Matchers.is;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 
 @Sql(value = "/data/category.sql")
 class CategoryControllerTest extends RestDocsSetting {
@@ -19,10 +19,10 @@ class CategoryControllerTest extends RestDocsSetting {
     void readRecipes() {
         RestAssured.given(spec).log().all()
                 .filter(document(DEFAULT_RESTDOCS_PATH,
-                        requestFields(
-                                fieldWithPath("category").description("카테고리"),
-                                fieldWithPath("pageNumber").description("페이지 번호"),
-                                fieldWithPath("pageSize").description("페이지 크기")
+                        queryParameters(
+                                parameterWithName("category").description("카테고리"),
+                                parameterWithName("pageNumber").description("페이지 번호"),
+                                parameterWithName("pageSize").description("페이지 크기")
                         )))
                 .queryParam("category", "한식")
                 .queryParam("pageNumber", 0)
