5e969f65759356f67723ab7dc749452e0cf6f40c
Hogu59 <dbstjdguswkd@naver.com>
2024-07-25 22:05:16 +0900

    :sparkles: implement DetailRecipeFragment

--- android/app/src/main/java/net/pengcook/android/presentation/detail/DetailRecipeViewModel.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/detail/DetailRecipeViewModel.kt
@@ -1,7 +1,7 @@
 package net.pengcook.android.presentation.detail
 
 import androidx.lifecycle.ViewModel
-import net.pengcook.android.domain.model.Recipe
+import net.pengcook.android.presentation.core.model.Recipe
 
 class DetailRecipeViewModel(
     private val recipe: Recipe,
