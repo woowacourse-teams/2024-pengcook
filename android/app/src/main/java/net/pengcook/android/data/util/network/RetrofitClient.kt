b5d30fe21ed6749c0d90a4ebd95679b159a0ea81
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-07-25 18:29:00 +0900

    :sparkles: implement retrofit client

---
+++ android/app/src/main/java/net/pengcook/android/data/util/network/RetrofitClient.kt
@@ -0,0 +1,16 @@
+package net.pengcook.android.data.util.network
+
+import net.pengcook.android.BuildConfig
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun <T> service(apiService: Class<T>): T {
+        return retrofit.create(apiService)
+    }
+}
