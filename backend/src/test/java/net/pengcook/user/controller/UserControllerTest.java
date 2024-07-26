9a0b2e0a6f733dde4a55338608bc6975835988f5
haiseong <wjdgotjd9908@gmail.com>
2024-07-26 09:55:48 +0900

    :recycle: change api end point

--- backend/src/test/java/net/pengcook/user/controller/UserControllerTest.java
+++ backend/src/test/java/net/pengcook/user/controller/UserControllerTest.java
@@ -5,10 +5,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.LocalDate;
+import net.pengcook.authentication.dto.TokenPayload;
+import net.pengcook.authentication.util.JwtTokenManager;
 import net.pengcook.user.dto.UserResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -20,6 +23,8 @@ class UserControllerTest {
 
     @LocalServerPort
     int port;
+    @Autowired
+    private JwtTokenManager jwtTokenManager;
 
     @BeforeEach
     void setUp() {
@@ -30,6 +35,7 @@ class UserControllerTest {
     @DisplayName("id를 통해 사용자의 정보를 조회한다.")
     void getUserById() {
         long id = 1L;
+        String accessToken = jwtTokenManager.createToken(new TokenPayload(id, "loki@pengcook.net"));
         UserResponse expected = new UserResponse(
                 id,
                 "loki@pengcook.net",
@@ -42,7 +48,8 @@ class UserControllerTest {
 
         UserResponse actual = RestAssured.given().log().all()
                 .contentType(ContentType.JSON)
-                .when().get("/api/user/" + id)
+                .header("Authorization", "Bearer " + accessToken)
+                .when().get("/api/user/me")
                 .then().log().all()
                 .statusCode(200)
                 .extract()
