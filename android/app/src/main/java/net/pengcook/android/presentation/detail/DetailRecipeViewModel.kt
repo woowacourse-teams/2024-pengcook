5eafeeadd3d15c417c740961c2176cb223723c0e
Hogu59 <dbstjdguswkd@naver.com>
2024-07-26 10:10:06 +0900

    :ambulance: fix navigation implementation

--- android/app/src/main/java/net/pengcook/android/presentation/detail/DetailRecipeViewModel.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/detail/DetailRecipeViewModel.kt
@@ -5,12 +5,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import net.pengcook.android.presentation.core.model.Recipe
+import net.pengcook.android.presentation.core.util.Event
 
-class DetailRecipeViewModel(private val recipe: Recipe) : ViewModel() {
-    private val _navigateToStepEvent = MutableLiveData<Boolean>()
-    val navigateToStepEvent: LiveData<Boolean> get() = _navigateToStepEvent
+class DetailRecipeViewModel(
+    private val recipe: Recipe,
+) : ViewModel() {
+    private val _navigateToStepEvent = MutableLiveData<Event<Boolean>>()
+    val navigateToStepEvent: LiveData<Event<Boolean>> get() = _navigateToStepEvent
 
     fun onNavigateToMakingStep() {
-        _navigateToStepEvent.value = true
+        _navigateToStepEvent.value = Event(true)
     }
 }
