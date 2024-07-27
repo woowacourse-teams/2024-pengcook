d8cb2fc266949975a8ba957b648180e7aec36fb1
haiseong <wjdgotjd9908@gmail.com>
2024-07-27 22:34:35 +0900

    :white_check_mark: add test code for restdocs

--- backend/src/test/java/net/pengcook/authentication/controller/LoginControllerTest.java
+++ backend/src/test/java/net/pengcook/authentication/controller/LoginControllerTest.java
@@ -4,6 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.document;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
@@ -11,7 +15,7 @@ import com.google.firebase.auth.FirebaseToken;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import java.time.LocalDate;
-import java.util.Map;
+import net.pengcook.RestDocsSetting;
 import net.pengcook.authentication.domain.JwtTokenManager;
 import net.pengcook.authentication.domain.TokenPayload;
 import net.pengcook.authentication.domain.TokenType;
@@ -19,32 +23,21 @@ import net.pengcook.authentication.dto.GoogleLoginRequest;
 import net.pengcook.authentication.dto.GoogleLoginResponse;
 import net.pengcook.authentication.dto.GoogleSignUpRequest;
 import net.pengcook.authentication.dto.GoogleSignUpResponse;
+import net.pengcook.authentication.dto.TokenRefreshRequest;
 import net.pengcook.authentication.dto.TokenRefreshResponse;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @Sql("/data/users.sql")
-class LoginControllerTest {
+class LoginControllerTest extends RestDocsSetting {
 
-    @LocalServerPort
-    int port;
     @MockBean
     private FirebaseAuth firebaseAuth;
     @Autowired
     private JwtTokenManager jwtTokenManager;
-
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
 
     @Test
     @DisplayName("이미 가입된 계정으로 로그인하면 이미 가입되었다고 알리고 access token과 refresh token을 반환한다.")
@@ -56,7 +49,17 @@ class LoginControllerTest {
         when(firebaseToken.getEmail()).thenReturn(email);
         when(firebaseAuth.verifyIdToken(idToken)).thenReturn(firebaseToken);
 
-        GoogleLoginResponse actual = RestAssured.given().log().all()
+        GoogleLoginResponse actual = RestAssured.given(spec).log().all()
+                .filter(document(DEFAULT_RESTDOCS_PATH,
+                        requestFields(
+                                fieldWithPath("idToken").description("Google ID Token")
+                        ),
+                        responseFields(
+                                fieldWithPath("accessToken").description("JWT Access Token"),
+                                fieldWithPath("refreshToken").description("JWT Refresh Token"),
+                                fieldWithPath("registered").description("사용자 등록 여부")
+                        )
+                ))
                 .contentType(ContentType.JSON)
                 .body(new GoogleLoginRequest(idToken))
                 .when().post("/api/oauth/google/login")
@@ -82,7 +85,17 @@ class LoginControllerTest {
         when(firebaseToken.getEmail()).thenReturn(email);
         when(firebaseAuth.verifyIdToken(idToken)).thenReturn(firebaseToken);
 
-        GoogleLoginResponse actual = RestAssured.given().log().all()
+        GoogleLoginResponse actual = RestAssured.given(spec).log().all()
+                .filter(document(DEFAULT_RESTDOCS_PATH,
+                        requestFields(
+                                fieldWithPath("idToken").description("Google ID Token")
+                        ),
+                        responseFields(
+                                fieldWithPath("accessToken").description("JWT Access Token").optional(),
+                                fieldWithPath("refreshToken").description("JWT Refresh Token").optional(),
+                                fieldWithPath("registered").description("사용자 등록 여부")
+                        )
+                ))
                 .contentType(ContentType.JSON)
                 .body(new GoogleLoginRequest(idToken))
                 .when().post("/api/oauth/google/login")
@@ -116,7 +129,27 @@ class LoginControllerTest {
         when(firebaseToken.getPicture()).thenReturn("new_face.jpg");
         when(firebaseAuth.verifyIdToken(idToken)).thenReturn(firebaseToken);
 
-        GoogleSignUpResponse actual = RestAssured.given().log().all()
+        GoogleSignUpResponse actual = RestAssured.given(spec).log().all()
+                .filter(document(DEFAULT_RESTDOCS_PATH,
+                        requestFields(
+                                fieldWithPath("idToken").description("Google ID Token"),
+                                fieldWithPath("username").description("사용자 아이디"),
+                                fieldWithPath("nickname").description("사용자 닉네임"),
+                                fieldWithPath("birthday").description("생년월일"),
+                                fieldWithPath("country").description("국가")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("사용자 ID"),
+                                fieldWithPath("email").description("사용자 이메일"),
+                                fieldWithPath("username").description("사용자 아이디"),
+                                fieldWithPath("nickname").description("사용자 닉네임"),
+                                fieldWithPath("image").description("사용자 프로필 이미지"),
+                                fieldWithPath("birthday").description("사용자 생년월일"),
+                                fieldWithPath("country").description("사용자 국가"),
+                                fieldWithPath("accessToken").description("JWT Access Token"),
+                                fieldWithPath("refreshToken").description("JWT Refresh Token")
+                        )
+                ))
                 .contentType(ContentType.JSON)
                 .body(request)
                 .when().post("/api/oauth/google/sign-up")
@@ -146,7 +179,16 @@ class LoginControllerTest {
         when(firebaseToken.getPicture()).thenReturn("loki.jpg");
         when(firebaseAuth.verifyIdToken(idToken)).thenReturn(firebaseToken);
 
-        RestAssured.given().log().all()
+        RestAssured.given(spec).log().all()
+                .filter(document(DEFAULT_RESTDOCS_PATH,
+                        requestFields(
+                                fieldWithPath("idToken").description("Google ID Token"),
+                                fieldWithPath("username").description("사용자 아이디"),
+                                fieldWithPath("nickname").description("사용자 닉네임"),
+                                fieldWithPath("birthday").description("생년월일"),
+                                fieldWithPath("country").description("국가")
+                        )
+                ))
                 .contentType(ContentType.JSON)
                 .body(request)
                 .when().post("/api/oauth/google/sign-up")
@@ -159,9 +201,18 @@ class LoginControllerTest {
     void refresh() {
         String refreshToken = jwtTokenManager.createToken(new TokenPayload(1L, "tester@pengcook.net", TokenType.REFRESH));
 
-        TokenRefreshResponse response = RestAssured.given().log().all()
+        TokenRefreshResponse response = RestAssured.given(spec).log().all()
+                .filter(document(DEFAULT_RESTDOCS_PATH,
+                        requestFields(
+                                fieldWithPath("refreshToken").description("JWT Refresh Token")
+                        ),
+                        responseFields(
+                                fieldWithPath("accessToken").description("새로 발급된 JWT Access Token"),
+                                fieldWithPath("refreshToken").description("새로 발급된 JWT Refresh Token")
+                        )
+                ))
                 .contentType(ContentType.JSON)
-                .body(Map.of("refreshToken", refreshToken))
+                .body(new TokenRefreshRequest(refreshToken))
                 .when().post("/api/token/refresh")
                 .then().log().all()
                 .statusCode(200)
