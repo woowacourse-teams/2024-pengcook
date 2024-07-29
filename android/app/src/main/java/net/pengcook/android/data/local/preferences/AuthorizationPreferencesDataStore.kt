d9f23923cfb4f832259d571815ad1b7debe93184
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-07-29 21:18:24 +0900

    :sparkles: implement PreferencesDatastore for storing tokens

---
+++ android/app/src/main/java/net/pengcook/android/data/local/preferences/AuthorizationPreferencesDataStore.kt
@@ -0,0 +1,10 @@
+package net.pengcook.android.data.local.preferences
+
+import android.content.Context
+import androidx.datastore.preferences.preferencesDataStore
+
+private const val AUTHORIZATION_PREFERENCES = "authorization_preferences"
+
+private val Context.dataStore by preferencesDataStore(
+    name = AUTHORIZATION_PREFERENCES,
+)
