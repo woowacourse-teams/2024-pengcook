d854433999b3a7581cc6a2c631817cc521a5d38b
ii2001 <iicks2001@gmail.com>
2024-07-31 15:49:30 +0900

    :sparkles: impl sending Image to AWS S3

---
+++ android/app/src/main/java/net/pengcook/android/data/remote/api/MakingRecipeService.kt
@@ -0,0 +1,27 @@
+package net.pengcook.android.data.remote.api
+
+import okhttp3.RequestBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Query
+import retrofit2.http.Url
+
+interface MakingRecipeService {
+
+    @GET("/image")
+    suspend fun fetchImageUri(
+        @Query("keyName") keyName: String
+    ): Response<PresignedUrlResponse>
+
+    @PUT
+    suspend fun uploadImageToS3(
+        @Url url: String,
+        @Body image: RequestBody
+    ): Response<Unit>
+}
+
+data class PresignedUrlResponse(
+    val url: String
+)
