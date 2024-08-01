ce57212dbec6ec28c67ebcc664ca354efd0941a9
hyun <osh123chm@gmail.com>
2024-08-01 19:17:52 +0900

    :recycle: add transaction to recipe service

--- backend/src/main/java/net/pengcook/recipe/service/RecipeService.java
+++ backend/src/main/java/net/pengcook/recipe/service/RecipeService.java
@@ -31,8 +31,10 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class RecipeService {
 
