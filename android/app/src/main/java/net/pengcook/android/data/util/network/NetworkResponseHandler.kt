83a6e06a13940c21aba35909d2a877f41e069d13
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-08-01 21:36:15 +0900

    :recycle: make network related objects abstract class

--- android/app/src/main/java/net/pengcook/android/data/util/network/NetworkResponseHandler.kt
+++ android/app/src/main/java/net/pengcook/android/data/util/network/NetworkResponseHandler.kt
@@ -2,9 +2,20 @@ package net.pengcook.android.data.util.network
 
 import retrofit2.Response
 
-interface NetworkResponseHandler {
+abstract class NetworkResponseHandler {
     fun <T> body(
         response: Response<T>,
-        validHttpCode: Int,
-    ): T
+        vararg validHttpCode: Int,
+    ): T {
+        val code = response.code()
+        val body = response.body()
+        if (code !in validHttpCode) throw RuntimeException(EXCEPTION_HTTP_CODE.format(code))
+        if (body == null) throw RuntimeException(EXCEPTION_NULL_BODY)
+        return body
+    }
+
+    companion object {
+        private const val EXCEPTION_HTTP_CODE = "Http code is not appropriate : %d"
+        private const val EXCEPTION_NULL_BODY = "Response body is null."
+    }
 }
--- android/app/src/main/java/net/pengcook/android/data/util/network/RetrofitClient.kt
+++ android/app/src/main/java/net/pengcook/android/data/util/network/RetrofitClient.kt
@@ -1,14 +1,29 @@
 package net.pengcook.android.data.util.network
 
+import java.util.concurrent.TimeUnit
 import net.pengcook.android.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
+    private val interceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val client = OkHttpClient.Builder().apply {
+        addInterceptor(interceptor)
+        connectTimeout(30, TimeUnit.SECONDS)
+        readTimeout(20, TimeUnit.SECONDS)
+        writeTimeout(25, TimeUnit.SECONDS)
+    }.build()
+
     private val retrofit =
         Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
 
     fun <T> service(apiService: Class<T>): T {
