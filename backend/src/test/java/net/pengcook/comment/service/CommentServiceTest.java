a11ff3c19f2c70ae72af393cfd9dc04aaa9d7aed
hyxrxn <gpfls981220@gmail.com>
2024-08-04 20:23:24 +0900

    :recycle: rename class for clarity

--- backend/src/test/java/net/pengcook/comment/service/CommentServiceTest.java
+++ backend/src/test/java/net/pengcook/comment/service/CommentServiceTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import java.util.List;
 import net.pengcook.authentication.domain.UserInfo;
-import net.pengcook.comment.dto.CommentResponse;
+import net.pengcook.comment.dto.CommentOfRecipeResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +25,14 @@ class CommentServiceTest {
     @DisplayName("레시피의 댓글을 조회한다.")
     void readComments() {
         UserInfo userInfo = new UserInfo(1, "ela@pengcook.net");
-        List<CommentResponse> expect = List.of(
-                new CommentResponse(1L, 2L, "loki.jpg", "loki", LocalDateTime.of(2024, 1, 1, 0, 0, 0), "great", false),
-                new CommentResponse(2L, 1L, "ela.jpg", "ela", LocalDateTime.of(2024, 1, 2, 0, 0, 0), "thank you", true)
+        List<CommentOfRecipeResponse> expect = List.of(
+                new CommentOfRecipeResponse(1L, 2L, "loki.jpg", "loki", LocalDateTime.of(2024, 1, 1, 0, 0, 0),
+                        "great", false),
+                new CommentOfRecipeResponse(2L, 1L, "ela.jpg", "ela", LocalDateTime.of(2024, 1, 2, 0, 0, 0),
+                        "thank you", true)
         );
 
-        List<CommentResponse> actual = commentService.readComments(1L, userInfo);
+        List<CommentOfRecipeResponse> actual = commentService.readComments(1L, userInfo);
 
         assertThat(actual).containsExactlyInAnyOrderElementsOf(expect);
     }
