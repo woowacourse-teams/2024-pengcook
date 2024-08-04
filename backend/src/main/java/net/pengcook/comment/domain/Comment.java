a0a29c8a263f7e96bd8b910b90383722093df026
hyxrxn <gpfls981220@gmail.com>
2024-08-04 20:07:31 +0900

    :recycle: set no args constructor to protected

--- backend/src/main/java/net/pengcook/comment/domain/Comment.java
+++ backend/src/main/java/net/pengcook/comment/domain/Comment.java
@@ -7,12 +7,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDateTime;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import net.pengcook.recipe.domain.Recipe;
 import net.pengcook.user.domain.User;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
     @Id
