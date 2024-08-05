7e471ff7513aefb75d3a1f04ab4a1708e529203f
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-08-05 22:55:48 +0900

    :sparkles: add layout and fragment classes for setting

---
+++ android/app/src/main/java/net/pengcook/android/presentation/setting/SettingViewHolder.kt
@@ -0,0 +1,25 @@
+package net.pengcook.android.presentation.setting
+
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.divider.MaterialDividerItemDecoration
+import net.pengcook.android.databinding.ItemSettingContainerBinding
+
+class SettingViewHolder(
+    private val binding: ItemSettingContainerBinding,
+    private val settingMenuItemClickListener: SettingMenuItemClickListener,
+) : RecyclerView.ViewHolder(binding.root) {
+    private val dividerItemDecoration =
+        MaterialDividerItemDecoration(binding.root.context, LinearLayout.VERTICAL).apply {
+            isLastItemDecorated = false
+        }
+
+    init {
+        binding.rvSettingContainer.addItemDecoration(dividerItemDecoration)
+    }
+
+    fun bind(item: Setting) {
+        val adapter = SettingItemRecyclerViewAdapter(item.menus, settingMenuItemClickListener)
+        binding.adapter = adapter
+    }
+}
