c7b328d309e9bcaa6b553e6e890664fa13715175
Gyeongho Yang <chchch1213@naver.com>
2024-08-05 17:34:46 +0900

    :white_check_mark: add reading likesCount function

--- backend/src/test/java/net/pengcook/like/controller/RecipeLikeControllerTest.java
+++ backend/src/test/java/net/pengcook/like/controller/RecipeLikeControllerTest.java
@@ -7,16 +7,37 @@ import net.pengcook.authentication.annotation.WithLoginUserTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.jdbc.Sql;
 
 import static com.epages.restdocs.apispec.RestAssuredRestDocumentationWrapper.document;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
-@Sql("/data/like.sql")
+@Sql(value = "/data/like.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @WithLoginUserTest
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RecipeLikeControllerTest extends RestDocsSetting {
+
+    @Test
+    @DisplayName("게시글의 좋아요 개수를 조회한다.")
+    void readLikesCount() {
+        RestAssured.given(spec).log().all()
+                .filter(document(DEFAULT_RESTDOCS_PATH,
+                        pathParameters(
+                                parameterWithName("recipeId").description("레시피 아이디")
+                        ),
+                        responseFields(
+                                fieldWithPath("likesCount").description("좋아요 개수")
+                        )))
+                .when().get("/api/likes/{recipeId}", 1L)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body("likesCount", is(1));
+    }
 
     @Test
     @WithLoginUser(email = "ela@pengcook.net")
@@ -30,6 +51,7 @@ class RecipeLikeControllerTest extends RestDocsSetting {
                                 parameterWithName("recipeId").description("레시피 아이디")
                         )))
                 .when().post("/api/likes/{recipeId}", 1L)
-                .then().log().all();
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
     }
 }
--- backend/src/test/resources/data/like.sql
+++ backend/src/test/resources/data/like.sql
@@ -9,8 +9,8 @@ TRUNCATE TABLE recipe;
 ALTER TABLE recipe
     ALTER COLUMN id RESTART;
 
-TRUNCATE TABLE comment;
-ALTER TABLE comment
+TRUNCATE TABLE recipe_like;
+ALTER TABLE recipe_like
     ALTER COLUMN id RESTART;
 
 SET
@@ -22,7 +22,7 @@ VALUES ('ela@pengcook.net', 'ela', '엘라', 'ela.jpg', 'KOREA'),
 
 INSERT INTO recipe (title, author_id, cooking_time, thumbnail, difficulty, like_count, description)
 VALUES ('김밥', 1, '01:00:00', '김밥이미지.jpg', 8, 1, '김밥 조리법'),
-       ('김치찌개', 1, '00:30:00', '김치찌개이미지.jpg', 3, 2, '김치찌개 조리법');
+       ('김치찌개', 1, '00:30:00', '김치찌개이미지.jpg', 3, 0, '김치찌개 조리법');
 
 INSERT INTO recipe_like (recipe_id, user_id)
 VALUES (1, 1);
