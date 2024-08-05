3af6cd43007c95a5280bd8969f89e986f3414a69
hyun <osh123chm@gmail.com>
2024-08-05 13:18:20 +0900

    :recycle: remove unnecessary annotation

--- backend/src/main/java/net/pengcook/recipe/controller/RecipeController.java
+++ backend/src/main/java/net/pengcook/recipe/controller/RecipeController.java
@@ -14,7 +14,6 @@ import net.pengcook.recipe.dto.RecipeStepRequest;
 import net.pengcook.recipe.dto.RecipeStepResponse;
 import net.pengcook.recipe.service.RecipeService;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +25,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Validated
 @RequestMapping("/api/recipes")
 public class RecipeController {
 
