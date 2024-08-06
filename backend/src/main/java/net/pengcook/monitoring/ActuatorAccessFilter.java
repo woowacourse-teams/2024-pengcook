9950a34ae0fb0f7eda01c5c4129af0f676d40210
Gyeongho Yang <chchch1213@naver.com>
2024-08-06 14:42:51 +0900

    :recycle: remove wildcard import on filter

--- backend/src/main/java/net/pengcook/monitoring/ActuatorAccessFilter.java
+++ backend/src/main/java/net/pengcook/monitoring/ActuatorAccessFilter.java
@@ -1,12 +1,15 @@
 package net.pengcook.monitoring;
 
-import jakarta.servlet.*;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Slf4j
 @Component
