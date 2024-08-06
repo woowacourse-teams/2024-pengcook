d0708a3549457f8ef1836695e15bc2cded5337bc
haiseong <wjdgotjd9908@gmail.com>
2024-08-06 17:32:02 +0900

    :fire: unnecessary newline

--- backend/src/main/java/net/pengcook/user/dto/UserBlockResponse.java
+++ backend/src/main/java/net/pengcook/user/dto/UserBlockResponse.java
@@ -1,7 +1,4 @@
 package net.pengcook.user.dto;
 
-public record UserBlockResponse(
-        UserResponse blocker,
-        UserResponse blockee
-) {
+public record UserBlockResponse(UserResponse blocker, UserResponse blockee) {
 }
