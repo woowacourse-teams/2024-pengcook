0e0f25fdcd01ad8c4bfa2aa6b757494420ba8e6e
hyun <osh123chm@gmail.com>
2024-08-06 18:33:04 +0900

    :recycle: add recipe fields to like test data

--- backend/src/test/resources/data/like.sql
+++ backend/src/test/resources/data/like.sql
@@ -20,9 +20,9 @@ INSERT INTO users (email, username, nickname, image, region)
 VALUES ('ela@pengcook.net', 'ela', '엘라', 'ela.jpg', 'KOREA'),
        ('loki@pengcook.net', 'loki', '로키', 'loki.jpg', 'KOREA');
 
-INSERT INTO recipe (title, author_id, cooking_time, thumbnail, difficulty, like_count, description)
-VALUES ('김밥', 1, '01:00:00', '김밥이미지.jpg', 8, 1, '김밥 조리법'),
-       ('김치찌개', 1, '00:30:00', '김치찌개이미지.jpg', 3, 0, '김치찌개 조리법');
+INSERT INTO recipe (title, author_id, cooking_time, thumbnail, difficulty, like_count, comment_count, description, created_at)
+VALUES ('김밥', 1, '01:00:00', '김밥이미지.jpg', 8, 1, 0, '김밥 조리법', '2024-07-02 13:00:00'),
+       ('김치찌개', 1, '00:30:00', '김치찌개이미지.jpg', 3, 0, 0, '김치찌개 조리법', '2024-07-02 13:00:00');
 
 INSERT INTO recipe_like (recipe_id, user_id)
 VALUES (1, 1);
