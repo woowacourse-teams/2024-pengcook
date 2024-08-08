aa6cd4946132006e902679be42ec9cfc02eb41c4
ii2001 <iicks2001@gmail.com>
2024-08-08 14:40:23 +0900

    :recycle: ktFormat

--- android/app/src/main/java/net/pengcook/android/data/datasource/like/DefaultLikeRemoteDataSource.kt
+++ android/app/src/main/java/net/pengcook/android/data/datasource/like/DefaultLikeRemoteDataSource.kt
@@ -1,8 +1,8 @@
 package net.pengcook.android.data.datasource.like
 
 import net.pengcook.android.data.model.like.IsLikeRequest
-import net.pengcook.android.data.remote.api.LikeService
 import net.pengcook.android.data.model.like.IsLikeResponse
+import net.pengcook.android.data.remote.api.LikeService
 import retrofit2.Response
 
 class DefaultLikeRemoteDataSource(
@@ -11,12 +11,10 @@ class DefaultLikeRemoteDataSource(
     override suspend fun fetchLikeCount(
         accessToken: String,
         recipeId: Long,
-    ): Response<IsLikeResponse> =
-        likeService.fetchLikeCount(accessToken, recipeId)
+    ): Response<IsLikeResponse> = likeService.fetchLikeCount(accessToken, recipeId)
 
     override suspend fun postLike(
         accessToken: String,
-        isLikeRequest: IsLikeRequest
-    ): Response<Unit> =
-        likeService.postLike(accessToken, isLikeRequest)
+        isLikeRequest: IsLikeRequest,
+    ): Response<Unit> = likeService.postLike(accessToken, isLikeRequest)
 }
