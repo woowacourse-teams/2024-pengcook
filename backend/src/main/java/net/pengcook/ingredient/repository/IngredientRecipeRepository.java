e82e1cb1a5008c2446e99f846364e666e5febab6
tackyu <tack970913@gmail.com>
2024-08-08 19:59:23 +0900

    :sparkles: delete ingredientRecipe by recipe

--- backend/src/main/java/net/pengcook/ingredient/repository/IngredientRecipeRepository.java
+++ backend/src/main/java/net/pengcook/ingredient/repository/IngredientRecipeRepository.java
@@ -1,7 +1,11 @@
 package net.pengcook.ingredient.repository;
 
+import java.util.List;
 import net.pengcook.ingredient.domain.IngredientRecipe;
+import net.pengcook.recipe.domain.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IngredientRecipeRepository extends JpaRepository<IngredientRecipe, Long> {
+
+    List<IngredientRecipe> findAllByRecipe(Recipe recipe);
 }
--- backend/src/main/java/net/pengcook/ingredient/repository/IngredientSubstitutionRepository.java
+++ backend/src/main/java/net/pengcook/ingredient/repository/IngredientSubstitutionRepository.java
@@ -1,7 +1,11 @@
 package net.pengcook.ingredient.repository;
 
+import java.util.List;
+import net.pengcook.ingredient.domain.IngredientRecipe;
 import net.pengcook.ingredient.domain.IngredientSubstitution;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IngredientSubstitutionRepository extends JpaRepository<IngredientSubstitution, Long> {
+
+    List<IngredientSubstitution> findAllByIngredientRecipe(IngredientRecipe ingredientRecipe);
 }
