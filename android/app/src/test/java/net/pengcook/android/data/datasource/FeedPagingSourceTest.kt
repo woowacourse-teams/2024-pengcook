8013e3e21ea6cad7aa1cb85d7763b05c5f0dab90
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-08-08 17:37:19 +0900

    :twisted_rightwards_arrows: resolve conflicts

--- android/app/src/test/java/net/pengcook/android/data/datasource/FeedPagingSourceTest.kt
+++ android/app/src/test/java/net/pengcook/android/data/datasource/FeedPagingSourceTest.kt
@@ -1,37 +1,27 @@
 package net.pengcook.android.data.datasource
 
-import androidx.paging.PagingConfig
 import androidx.paging.PagingSource
-import androidx.paging.PagingState
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import net.pengcook.android.data.repository.feed.FeedRepository
 import net.pengcook.android.presentation.core.model.Recipe
 import net.pengcook.android.presentation.core.model.User
-import net.pengcook.android.presentation.search.SearchEventListener
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.Test
 
-class SearchPagingSourceTest {
+class FeedPagingSourceTest {
     private lateinit var feedRepository: FeedRepository
-    private lateinit var searchEventListener: SearchEventListener
-    private lateinit var pagingSource: SearchPagingSource
-
-    @Before
-    fun setup() {
-        searchEventListener = mockk(relaxed = true)
-    }
+    private lateinit var pagingSource: FeedPagingSource
 
     @Test
     fun `load returns Page on successful load`() =
         runTest {
             feedRepository = mockk()
-            pagingSource = SearchPagingSource(feedRepository, searchEventListener)
+            pagingSource = FeedPagingSource(feedRepository)
             val recipes = recipes()
 
-            coEvery { feedRepository.fetchRecipes(any(), any()) } returns Result.success(recipes)
+            coEvery { feedRepository.fetchRecipes(any(), any(), any(), any(), any()) } returns Result.success(recipes)
 
             val params =
                 PagingSource.LoadParams.Refresh<Int>(
@@ -48,30 +38,6 @@ class SearchPagingSourceTest {
             assertEquals(1, page.nextKey)
         }
 
-    @Test
-    fun `getRefreshKey returns correct key`() {
-        feedRepository = mockk()
-        pagingSource = SearchPagingSource(feedRepository, searchEventListener)
-        val state =
-            PagingState(
-                pages =
-                    listOf(
-                        PagingSource.LoadResult.Page(
-                            data = recipes(),
-                            prevKey = null,
-                            nextKey = 1,
-                        ),
-                    ),
-                anchorPosition = 1,
-                config = PagingConfig(pageSize = 2),
-                leadingPlaceholderCount = 0,
-            )
-
-        val key = pagingSource.getRefreshKey(state)
-
-        assertEquals(0, key)
-    }
-
     private fun recipes() =
         listOf(
             Recipe(
@@ -81,7 +47,7 @@ class SearchPagingSourceTest {
                 cookingTime = "30 min",
                 thumbnail = "",
                 user = User(1, "User1", "profile1"),
-                favoriteCount = 100,
+                likeCount = 100,
                 ingredients = listOf(),
                 difficulty = 1,
                 introduction = "Delicious fish cake",
@@ -94,7 +60,7 @@ class SearchPagingSourceTest {
                 cookingTime = "45 min",
                 thumbnail = "",
                 user = User(2, "User2", "profile2"),
-                favoriteCount = 150,
+                likeCount = 150,
                 ingredients = listOf(),
                 difficulty = 2,
                 introduction = "Healthy chicken soup",
