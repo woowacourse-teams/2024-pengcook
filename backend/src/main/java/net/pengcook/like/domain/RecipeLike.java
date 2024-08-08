706a06de125fd194d86f36b94117b74e82b6e89a
Gyeongho Yang <chchch1213@naver.com>
2024-08-08 11:44:48 +0900

    :recycle: remove wildcard import on RecipeLike

--- backend/src/main/java/net/pengcook/like/domain/RecipeLike.java
+++ backend/src/main/java/net/pengcook/like/domain/RecipeLike.java
@@ -1,6 +1,13 @@
 package net.pengcook.like.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
