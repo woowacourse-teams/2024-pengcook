56c00af51a9d0e8142bea938030a7952a2cf938f
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-08-20 17:58:41 +0900

    :sparkles: add navigation to terms and privacy policy

--- android/app/src/main/java/net/pengcook/android/presentation/setting/SettingFragment.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/setting/SettingFragment.kt
@@ -1,5 +1,7 @@
 package net.pengcook.android.presentation.setting
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -65,8 +67,8 @@ class SettingFragment :
             MenuItem.COMMENTS -> {}
             MenuItem.BLOCKED -> {}
             MenuItem.LANGUAGE -> {}
-            MenuItem.PRIVACY_POLICY -> {}
-            MenuItem.TERMS_OF_USE -> {}
+            MenuItem.PRIVACY_POLICY -> navigateToBrowser("https://pengcook.net/privacy-policy")
+            MenuItem.TERMS_OF_USE -> navigateToBrowser("https://pengcook.net/terms-of-service")
             MenuItem.ACCOUNT ->
                 findNavController().navigate(R.id.action_settingFragment_to_accountSettingFragment)
             else -> Unit
@@ -75,5 +77,9 @@ class SettingFragment :
 
     override fun onNavigateBack() {
         findNavController().navigateUp()
+    }
+
+    private fun navigateToBrowser(uriString: String) {
+        Intent(Intent.ACTION_VIEW, Uri.parse(uriString)).also(::startActivity)
     }
 }
