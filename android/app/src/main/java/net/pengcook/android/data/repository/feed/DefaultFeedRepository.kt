9a5c295fb35d74afc39b9087aaf1018c2889da0f
Hogu59 <dbstjdguswkd@naver.com>
2024-08-20 17:17:39 +0900

    :sparkles: add delete of feed (repository)

--- android/app/src/main/java/net/pengcook/android/data/repository/feed/DefaultFeedRepository.kt
+++ android/app/src/main/java/net/pengcook/android/data/repository/feed/DefaultFeedRepository.kt
@@ -1,10 +1,10 @@
 package net.pengcook.android.data.repository.feed
 
 import kotlinx.coroutines.flow.first
+import net.pengcook.android.data.datasource.auth.SessionLocalDataSource
 import net.pengcook.android.data.datasource.feed.FeedRemoteDataSource
 import net.pengcook.android.data.model.feed.item.FeedItemResponse
 import net.pengcook.android.data.model.step.RecipeStepResponse
-import net.pengcook.android.data.repository.auth.SessionRepository
 import net.pengcook.android.data.util.mapper.toRecipe
 import net.pengcook.android.data.util.mapper.toRecipeStep
 import net.pengcook.android.data.util.network.NetworkResponseHandler
@@ -12,10 +12,10 @@ import net.pengcook.android.presentation.core.model.Recipe
 import net.pengcook.android.presentation.core.model.RecipeStep
 
 class DefaultFeedRepository(
-    private val sessionRepository: SessionRepository,
+    private val sessionLocalDataSource: SessionLocalDataSource,
     private val feedRemoteDataSource: FeedRemoteDataSource,
-) : FeedRepository,
-    NetworkResponseHandler() {
+) : NetworkResponseHandler(),
+    FeedRepository {
     override suspend fun fetchRecipes(
         pageNumber: Int,
         pageSize: Int,
@@ -24,8 +24,7 @@ class DefaultFeedRepository(
         userId: Long?,
     ): Result<List<Recipe>> =
         runCatching {
-            val accessToken =
-                sessionRepository.sessionData.first().accessToken ?: throw RuntimeException()
+            val accessToken = sessionLocalDataSource.sessionData.first().accessToken ?: throw RuntimeException()
             val response =
                 feedRemoteDataSource.fetchRecipes(accessToken, pageNumber, pageSize, category, keyword, userId)
             body(response, RESPONSE_CODE_SUCCESS).map(FeedItemResponse::toRecipe)
@@ -37,7 +36,15 @@ class DefaultFeedRepository(
             body(response, RESPONSE_CODE_SUCCESS).map(RecipeStepResponse::toRecipeStep)
         }
 
+    override suspend fun deleteRecipe(recipeId: Long): Result<Unit> =
+        runCatching {
+            val accessToken = sessionLocalDataSource.sessionData.first().accessToken ?: throw RuntimeException()
+            val response = feedRemoteDataSource.deleteRecipe(accessToken, recipeId)
+            body(response, RESPONSE_CODE_DELETED)
+        }
+
     companion object {
         private const val RESPONSE_CODE_SUCCESS = 200
+        private const val RESPONSE_CODE_DELETED = 204
     }
 }
