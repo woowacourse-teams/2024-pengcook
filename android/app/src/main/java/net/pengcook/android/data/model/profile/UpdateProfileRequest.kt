3bf615db16bda6c808d1cefc927c6d17b591fb3c
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-08-20 20:48:19 +0900

    :sparkles: implement profile patching

--- android/app/src/main/java/net/pengcook/android/data/model/profile/UpdateProfileRequest.kt
+++ android/app/src/main/java/net/pengcook/android/data/model/profile/UpdateProfileRequest.kt
@@ -3,7 +3,7 @@ package net.pengcook.android.data.model.profile
 data class UpdateProfileRequest(
     val username: String,
     val nickname: String,
-    val image: String,
+    val image: String?,
     val region: String,
     val introduction: String,
 )
