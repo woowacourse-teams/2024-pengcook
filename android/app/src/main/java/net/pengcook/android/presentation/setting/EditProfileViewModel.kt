d72c3884b768477d739e4de1fdd7cf063aa718a1
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-08-20 22:08:04 +0900

    :fix: fix edit profile

---
+++ android/app/src/main/java/net/pengcook/android/presentation/setting/EditProfileViewModel.kt
@@ -0,0 +1,191 @@
+package net.pengcook.android.presentation.setting
+
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import net.pengcook.android.data.model.profile.UpdateProfileRequest
+import net.pengcook.android.data.repository.auth.AuthorizationRepository
+import net.pengcook.android.data.repository.photo.ImageRepository
+import net.pengcook.android.data.repository.profile.ProfileRepository
+import net.pengcook.android.domain.usecase.ValidateNicknameUseCase
+import net.pengcook.android.domain.usecase.ValidateUsernameUseCase
+import net.pengcook.android.presentation.core.listener.AppbarSingleActionEventListener
+import net.pengcook.android.presentation.core.listener.SpinnerItemChangeListener
+import net.pengcook.android.presentation.core.util.Event
+import net.pengcook.android.presentation.signup.BottomButtonClickListener
+import java.io.File
+
+class EditProfileViewModel(
+    private val authorizationRepository: AuthorizationRepository,
+    private val imageRepository: ImageRepository,
+    private val profileRepository: ProfileRepository,
+    private val validateUsernameUseCase: ValidateUsernameUseCase = ValidateUsernameUseCase(),
+    private val validateNicknameUseCase: ValidateNicknameUseCase = ValidateNicknameUseCase(),
+) : ViewModel(),
+    BottomButtonClickListener,
+    SpinnerItemChangeListener,
+    AppbarSingleActionEventListener {
+    val usernameContent: MutableLiveData<String> = MutableLiveData()
+    val nicknameContent: MutableLiveData<String> = MutableLiveData()
+    val country: MutableLiveData<String> = MutableLiveData()
+
+    private val _imageUri: MutableLiveData<Uri> = MutableLiveData()
+    val imageUri: LiveData<Uri>
+        get() = _imageUri
+
+    private val _imageUploaded: MutableLiveData<Boolean> = MutableLiveData(false)
+    val imageUploaded: LiveData<Boolean>
+        get() = _imageUploaded
+
+    private val _imageSelected: MutableLiveData<Boolean> = MutableLiveData(false)
+    val imageSelected: LiveData<Boolean>
+        get() = _imageSelected
+
+    private val _isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isLoading: LiveData<Boolean>
+        get() = _isLoading
+
+    private val _signUpEvent: MutableLiveData<Event<EditProfileEvent>> = MutableLiveData()
+    val signUpEvent: LiveData<Event<EditProfileEvent>>
+        get() = _signUpEvent
+
+    private var imageTitle: String? = null
+
+    init {
+        viewModelScope.launch {
+            profileRepository.fetchMyUserInformation()
+                .onSuccess { userProfile ->
+                    usernameContent.value = userProfile.username
+                    nicknameContent.value = userProfile.nickname
+                    country.value = userProfile.region
+                    imageTitle = userProfile.image
+                    _imageUri.value = Uri.parse(userProfile.image)
+                }.onFailure {
+                    _signUpEvent.value = Event(EditProfileEvent.Error)
+                }
+        }
+    }
+
+    fun fetchImageUri(keyName: String) {
+        viewModelScope.launch {
+            _imageSelected.value = true
+            imageRepository.fetchImageUri(keyName)
+                .onSuccess { uri ->
+                    _signUpEvent.value = Event(EditProfileEvent.PresignedUrlRequestSuccessful(uri))
+                }.onFailure {
+                    _signUpEvent.value = Event(EditProfileEvent.PostImageFailure)
+                }
+        }
+    }
+
+    fun uploadImageToS3(
+        presignedUrl: String,
+        file: File,
+    ) {
+        viewModelScope.launch {
+            imageRepository.uploadImage(presignedUrl, file)
+                .onSuccess {
+                    imageTitle = file.name
+                    _signUpEvent.value = Event(EditProfileEvent.PostImageSuccessful)
+                    _imageUploaded.value = true
+                }.onFailure {
+                    _signUpEvent.value = Event(EditProfileEvent.PostImageFailure)
+                }
+        }
+    }
+
+    fun changeCurrentImage(uri: Uri) {
+        _imageUri.value = uri
+    }
+
+    fun addImage() {
+        _signUpEvent.value = Event(EditProfileEvent.AddImage)
+    }
+
+    override fun onConfirm() {
+        viewModelScope.launch {
+            val username = usernameContent.value
+            val nickname = nicknameContent.value
+            val country = country.value
+
+            if (username.isNullOrEmpty() || nickname.isNullOrEmpty() || country.isNullOrEmpty()) {
+                _signUpEvent.value = Event(EditProfileEvent.FormNotCompleted)
+                return@launch
+            }
+            if (!formValid(username, nickname)) return@launch
+            if (!usernameAvailable(username)) return@launch
+
+            _isLoading.value = true
+            editProfile(country, nickname, username)
+        }
+    }
+
+    override fun onSelectionChange(item: String) {
+        country.value = item
+    }
+
+    override fun onNavigateBack() {
+        _signUpEvent.value = Event(EditProfileEvent.BackPressed)
+    }
+
+    private fun onEditProfileFailure() {
+        _isLoading.value = false
+        _signUpEvent.value = Event(EditProfileEvent.Error)
+    }
+
+    private fun formValid(
+        username: String,
+        nickname: String,
+    ): Boolean {
+        if (!validateUsernameUseCase(username)) {
+            _signUpEvent.value = Event(EditProfileEvent.UsernameInvalid)
+            return false
+        }
+
+        if (!validateNicknameUseCase(nickname)) {
+            _signUpEvent.value = Event(EditProfileEvent.NicknameLengthInvalid)
+            return false
+        }
+
+        return true
+    }
+
+    private suspend fun editProfile(
+        country: String,
+        nickname: String,
+        username: String,
+    ) {
+        profileRepository
+            .patchMyUserInformation(
+                UpdateProfileRequest(username, nickname, imageTitle, country, ""),
+            ).onSuccess {
+                onEditSuccessful()
+            }.onFailure {
+                onEditProfileFailure()
+            }
+    }
+
+    private suspend fun usernameAvailable(username: String): Boolean {
+        authorizationRepository
+            .checkUsernameDuplication(username)
+            .onSuccess { available ->
+                if (!available) {
+                    _signUpEvent.value = Event(EditProfileEvent.NicknameDuplicated)
+                    return false
+                }
+            }.onFailure {
+                _signUpEvent.value = Event(EditProfileEvent.Error)
+                return false
+            }
+
+        return true
+    }
+
+    private fun onEditSuccessful() {
+        _isLoading.value = false
+        _signUpEvent.value = Event(EditProfileEvent.EditProfileSuccessful)
+    }
+}
