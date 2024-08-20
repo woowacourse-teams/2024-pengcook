30b378145da40b3f8c3469190cebf776be759fb2
Hogu59 <dbstjdguswkd@naver.com>
2024-08-20 16:57:01 +0900

    :sparkles: data layer of UserControl (dataSource)

---
+++ android/app/src/main/java/net/pengcook/android/data/datasource/usercontrol/DefaultUserControlDataSource.kt
@@ -0,0 +1,26 @@
+package net.pengcook.android.data.datasource.usercontrol
+
+import net.pengcook.android.data.model.usercontrol.BlockUserRequest
+import net.pengcook.android.data.model.usercontrol.ReportReasonResponse
+import net.pengcook.android.data.model.usercontrol.ReportResponse
+import net.pengcook.android.data.model.usercontrol.ReportUserRequest
+import net.pengcook.android.data.remote.api.UserControlService
+import net.pengcook.android.data.util.network.NetworkResponseHandler
+import retrofit2.Response
+
+class DefaultUserControlDataSource(
+    private val userControlService: UserControlService,
+) : NetworkResponseHandler(),
+    UserControlDataSource {
+    override suspend fun blockUser(
+        accessToken: String,
+        blockUserRequest: BlockUserRequest,
+    ): Response<Unit> = userControlService.blockUser(accessToken, blockUserRequest)
+
+    override suspend fun fetchReportReasons(): Response<List<ReportReasonResponse>> = userControlService.fetchReportReasons()
+
+    override suspend fun reportUser(
+        accessToken: String,
+        reportUserRequest: ReportUserRequest,
+    ): Response<ReportResponse> = userControlService.reportUser(accessToken, reportUserRequest)
+}
---
+++ android/app/src/main/java/net/pengcook/android/data/datasource/usercontrol/FakeUserControlDataSource.kt
@@ -0,0 +1,45 @@
+package net.pengcook.android.data.datasource.usercontrol
+
+import net.pengcook.android.data.model.usercontrol.BlockUserRequest
+import net.pengcook.android.data.model.usercontrol.ReportReasonResponse
+import net.pengcook.android.data.model.usercontrol.ReportResponse
+import net.pengcook.android.data.model.usercontrol.ReportUserRequest
+import retrofit2.Response
+
+class FakeUserControlDataSource : UserControlDataSource {
+    override suspend fun blockUser(
+        accessToken: String,
+        blockUserRequest: BlockUserRequest,
+    ): Response<Unit> = Response.success(Unit)
+
+    override suspend fun fetchReportReasons(): Response<List<ReportReasonResponse>> =
+        Response.success(
+            listOf(
+                ReportReasonResponse(
+                    reason = "SPAM_CONTENT",
+                    message = "Excessive posting or spamming",
+                ),
+                ReportReasonResponse(
+                    reason = "INAPPROPRIATE_CONTENT",
+                    message = "Contains inappropriate content",
+                ),
+            ),
+        )
+
+    override suspend fun reportUser(
+        accessToken: String,
+        reportUserRequest: ReportUserRequest,
+    ): Response<ReportResponse> =
+        Response.success(
+            ReportResponse(
+                reportId = 1,
+                reporterId = 1,
+                reporteeId = reportUserRequest.reporteeId,
+                reason = reportUserRequest.reason,
+                type = reportUserRequest.type,
+                targetId = reportUserRequest.targetId,
+                details = reportUserRequest.details,
+                createdAt = System.currentTimeMillis().toString(),
+            ),
+        )
+}
