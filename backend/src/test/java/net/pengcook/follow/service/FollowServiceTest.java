2bd6378a80efdab70b546bd6bc2cfe2bd99397a1
Gyeongho Yang <chchch1213@naver.com>
2024-08-20 14:31:46 +0900

    :sparkles: add follow test sql

--- backend/src/test/java/net/pengcook/follow/service/FollowServiceTest.java
+++ backend/src/test/java/net/pengcook/follow/service/FollowServiceTest.java
@@ -1,15 +1,40 @@
 package net.pengcook.follow.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import net.pengcook.authentication.domain.UserInfo;
+import net.pengcook.follow.entity.Follow;
+import net.pengcook.follow.repository.FollowRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
+@Sql("/data/follow.sql")
+@Import({FollowService.class})
 @DataJpaTest
 class FollowServiceTest {
+
+    @Autowired
+    FollowService followService;
+    @Autowired
+    FollowRepository followRepository;
 
     @Test
     @DisplayName("한 사람이 다른 사람을 팔로우 한다.")
     void follow() {
+        UserInfo userInfo = new UserInfo(1, "ela@pengcook.net");
+        int followeeId = 2;
 
+        followService.follow(userInfo, followeeId);
+        Follow lastFollow = followRepository.findAll().getLast();
+
+        assertAll(
+                () -> assertThat(lastFollow.getFollower().getId()).isEqualTo(userInfo.getId()),
+                () -> assertThat(lastFollow.getFollowee().getId()).isEqualTo(followeeId)
+        );
     }
 }
--- backend/src/test/resources/data/follow.sql
+++ backend/src/test/resources/data/follow.sql
@@ -1,5 +1,4 @@
-SET
-REFERENTIAL_INTEGRITY FALSE;
+SET REFERENTIAL_INTEGRITY FALSE;
 
 TRUNCATE TABLE users;
 ALTER TABLE users
@@ -9,10 +8,12 @@ TRUNCATE TABLE follow;
 ALTER TABLE follow
     ALTER COLUMN id RESTART;
 
-SET
-REFERENTIAL_INTEGRITY TRUE;
+SET REFERENTIAL_INTEGRITY TRUE;
 
 INSERT INTO users (email, username, nickname, image, region)
 VALUES ('ela@pengcook.net', 'ela', '엘라', 'ela.jpg', 'KOREA'),
        ('loki@pengcook.net', 'loki', '로키', 'loki.jpg', 'KOREA'),
        ('seyang@pengcook.net', 'seyang', '새양', 'seyang.jpg', 'GERMANY');
+
+INSERT INTO follow (followee_id, follower_id)
+VALUES (2, 1);
