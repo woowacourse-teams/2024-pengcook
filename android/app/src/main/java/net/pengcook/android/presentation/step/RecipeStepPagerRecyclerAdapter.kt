6444ae848890ed5a1e07055c3ae37cbfba402362
ii2001 <iicks2001@gmail.com>
2024-09-19 13:01:33 +0900

    :sparkles: timer feature

--- android/app/src/main/java/net/pengcook/android/presentation/step/RecipeStepPagerRecyclerAdapter.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/step/RecipeStepPagerRecyclerAdapter.kt
@@ -1,7 +1,6 @@
 package net.pengcook.android.presentation.step
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
@@ -12,12 +11,14 @@ class RecipeStepPagerRecyclerAdapter(
     private var pageList: List<RecipeStep> = emptyList(),
     private val viewPager2: ViewPager2,
 ) : RecyclerView.Adapter<RecipeStepViewHolder>() {
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
     ): RecipeStepViewHolder {
-        val binding =
-            ItemStepRecipeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding = ItemStepRecipeBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
         return RecipeStepViewHolder(binding)
     }
 
@@ -27,20 +28,17 @@ class RecipeStepPagerRecyclerAdapter(
         holder: RecipeStepViewHolder,
         position: Int,
     ) {
-        holder.bind(pageList[position], createOnClickListener())
-    }
-
-    private fun createOnClickListener(): View.OnClickListener {
-        return View.OnClickListener {
+        val moveToNextStep = {
             val currentItem = viewPager2.currentItem
             if (currentItem < itemCount - 1) {
                 viewPager2.currentItem = currentItem + 1
             }
         }
+        holder.bind(pageList[position], moveToNextStep)
     }
 
     fun updateList(list: List<RecipeStep>) {
         pageList = list
-        notifyItemInserted(pageList.size)
+        notifyDataSetChanged()
     }
 }
