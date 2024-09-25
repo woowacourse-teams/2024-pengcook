3bfd9dcb884b25c79c87b06a8df253abeac2e531
Gyeongho Yang <chchch1213@naver.com>
2024-09-25 15:42:59 +0900

    :sparkles: add unique key on recipe_id and sequence

--- backend/src/main/java/net/pengcook/recipe/domain/RecipeStep.java
+++ backend/src/main/java/net/pengcook/recipe/domain/RecipeStep.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import java.time.LocalTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,9 +15,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"recipe_id", "sequence"})})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Getter
 public class RecipeStep {
 
     @Id
