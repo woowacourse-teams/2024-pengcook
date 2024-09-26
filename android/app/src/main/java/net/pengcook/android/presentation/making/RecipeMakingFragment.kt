f20f5b1233be121327732d9bd16446434cb5e0c1
ii2001 <iicks2001@gmail.com>
2024-09-26 17:06:30 +0900

    :fire: unused Dispatchers

--- android/app/src/main/java/net/pengcook/android/presentation/making/RecipeMakingFragment.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/making/RecipeMakingFragment.kt
@@ -130,9 +130,7 @@ class RecipeMakingFragment : Fragment() {
                 val compressedFile = imageUtils.compressAndResizeImage(uri)
                 currentPhotoPath = compressedFile.absolutePath
 
-                withContext(Dispatchers.Main) {
-                    viewModel.fetchImageUri(File(currentPhotoPath!!).name)
-                }
+                viewModel.fetchImageUri(File(currentPhotoPath!!).name)
             } catch (e: IOException) {
                 e.printStackTrace()
                 withContext(Dispatchers.Main) {
