96304c7c9a326e47a25b33d4c70fd5344ab31128
ii2001 <iicks2001@gmail.com>
2024-09-26 13:59:11 +0900

    :sparkles: refresh in Home

--- android/app/src/main/java/net/pengcook/android/presentation/home/HomeViewModel.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/home/HomeViewModel.kt
@@ -8,8 +8,8 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import androidx.paging.liveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import net.pengcook.android.data.datasource.FeedPagingSource
 import net.pengcook.android.data.repository.feed.FeedRepository
 import net.pengcook.android.presentation.core.model.Recipe
@@ -22,18 +22,38 @@ class HomeViewModel
     @Inject
     constructor(
         private val feedRepository: FeedRepository,
-    ) : ViewModel(), FeedItemEventListener {
+    ) : ViewModel(),
+        FeedItemEventListener {
         private val _uiEvent: MutableLiveData<Event<HomeEvent>> = MutableLiveData()
         val uiEvent: LiveData<Event<HomeEvent>>
             get() = _uiEvent
 
-        val feedData: LiveData<PagingData<Recipe>> =
-            Pager(
-                config = PagingConfig(pageSize = PAGE_SIZE, enablePlaceholders = false),
-                pagingSourceFactory = { FeedPagingSource(feedRepository) },
-            )
-                .liveData
-                .cachedIn(viewModelScope)
+        private val _feedData = MutableLiveData<PagingData<Recipe>>()
+        val feedData: LiveData<PagingData<Recipe>> = _feedData
+
+        init {
+            loadFeedData()
+        }
+
+        private fun loadFeedData() {
+            val pager =
+                Pager(
+                    config = PagingConfig(pageSize = PAGE_SIZE, enablePlaceholders = false),
+                    pagingSourceFactory = { FeedPagingSource(feedRepository) },
+                )
+
+            viewModelScope.launch {
+                pager.flow
+                    .cachedIn(viewModelScope)
+                    .collect { pagingData ->
+                        _feedData.value = pagingData
+                    }
+            }
+        }
+
+        fun refreshFeed() {
+            loadFeedData()
+        }
 
         override fun onNavigateToDetail(recipe: Recipe) {
             _uiEvent.value = Event(HomeEvent.NavigateToDetail(recipe))
@@ -45,5 +65,7 @@ class HomeViewModel
     }
 
 sealed interface HomeEvent {
-    data class NavigateToDetail(val recipe: Recipe) : HomeEvent
+    data class NavigateToDetail(
+        val recipe: Recipe,
+    ) : HomeEvent
 }
