497daab5be01ad5e3239094f528f3ee8244bba44
tackyu <tack970913@gmail.com>
2024-12-30 15:12:59 +0900

    :white_check_mark: UserFollow creation test

--- backend/src/test/java/net/pengcook/user/domain/UserFollowTest.java
+++ backend/src/test/java/net/pengcook/user/domain/UserFollowTest.java
@@ -1,12 +1,28 @@
 package net.pengcook.user.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import net.pengcook.user.exception.BadArgumentException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserFollowTest {
+
+    @Test
+    @DisplayName("UserFollow 객체를 생성한다")
+    void createUserFollow() {
+        User follower = new User(1L, "email1", "user1", "nick1", "image1", "region", 0, 0);
+        User followee = new User(2L, "email2", "user2", "nick2", "image2", "region", 0, 0);
+
+        UserFollow userFollow = new UserFollow(follower, followee);
+
+        assertAll(
+                () -> assertThat(userFollow.getFollower()).isEqualTo(follower),
+                () -> assertThat(userFollow.getFollowee()).isEqualTo(followee)
+        );
+    }
 
     @Test
     @DisplayName("자기 자신을 팔로우하면 예외가 발생한다.")
