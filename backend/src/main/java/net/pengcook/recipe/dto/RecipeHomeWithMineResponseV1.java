e6d047e35cafd3b0106d12714113b549f645947e
tackyu <tack970913@gmail.com>
2025-02-02 18:18:40 +0900

    :recycle: remove unnecessary conversion to RecipeHomeResponse

--- backend/src/main/java/net/pengcook/recipe/dto/RecipeHomeWithMineResponseV1.java
+++ backend/src/main/java/net/pengcook/recipe/dto/RecipeHomeWithMineResponseV1.java
@@ -2,6 +2,7 @@ package net.pengcook.recipe.dto;
 
 import java.time.LocalDateTime;
 import net.pengcook.authentication.domain.UserInfo;
+import net.pengcook.recipe.domain.Recipe;
 
 public record RecipeHomeWithMineResponseV1(
         long recipeId,
@@ -27,6 +28,22 @@ public record RecipeHomeWithMineResponseV1(
                 firstResponse.commentCount(),
                 firstResponse.createdAt(),
                 userInfo.isSameUser(firstResponse.authorId())
+        );
+    }
+
+    public RecipeHomeWithMineResponseV1(
+            UserInfo userInfo,
+            Recipe recipe
+    ) {
+        this(
+                recipe.getId(),
+                recipe.getTitle(),
+                new AuthorResponse(recipe.getAuthor()),
+                recipe.getThumbnail(),
+                recipe.getLikeCount(),
+                recipe.getCommentCount(),
+                recipe.getCreatedAt(),
+                userInfo.isSameUser(recipe.getAuthor().getId())
         );
     }
 }
