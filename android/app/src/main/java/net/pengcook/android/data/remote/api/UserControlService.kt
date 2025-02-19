5a479401940669ae220122e9c2a0344d18520a9f
Hogu59 <dbstjdguswkd@naver.com>
2025-02-19 10:59:07 +0900

    :sparkles: add follow and unfollow api calls

--- android/app/src/main/java/net/pengcook/android/data/remote/api/UserControlService.kt
+++ android/app/src/main/java/net/pengcook/android/data/remote/api/UserControlService.kt
@@ -1,12 +1,14 @@
 package net.pengcook.android.data.remote.api
 
 import net.pengcook.android.data.model.usercontrol.BlockUserRequest
+import net.pengcook.android.data.model.usercontrol.FollowUserRequest
 import net.pengcook.android.data.model.usercontrol.ReportReasonResponse
 import net.pengcook.android.data.model.usercontrol.ReportResponse
 import net.pengcook.android.data.model.usercontrol.ReportUserRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.HTTP
 import retrofit2.http.Header
 import retrofit2.http.POST
 
@@ -25,4 +27,16 @@ interface UserControlService {
         @Header("Authorization") accessToken: String,
         @Body reportUserRequest: ReportUserRequest,
     ): Response<ReportResponse>
+
+    @POST("/user/follow")
+    suspend fun followUser(
+        @Header("Authorization") accessToken: String,
+        @Body followUserRequest: FollowUserRequest,
+    ): Response<Unit>
+
+    @HTTP(method = "DELETE", path = "/user/follow", hasBody = true)
+    suspend fun unfollowUser(
+        @Header("Authorization") accessToken: String,
+        @Body followUserRequest: FollowUserRequest,
+    ): Response<Unit>
 }
