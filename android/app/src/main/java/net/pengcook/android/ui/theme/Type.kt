39a6c8f5d3898bffa1558064cf8964db674906eb
Hogu59 <dbstjdguswkd@naver.com>
2025-02-21 11:02:25 +0900

    :lipstick: apply theme (Typo, Color)

--- android/app/src/main/java/net/pengcook/android/ui/theme/Type.kt
+++ android/app/src/main/java/net/pengcook/android/ui/theme/Type.kt
@@ -2,11 +2,24 @@ package net.pengcook.android.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import net.pengcook.android.R
 
 // Set of Material typography styles to start with
+private val NotoSansFontFamily = FontFamily(
+    Font(R.font.notosans_regular, FontWeight.Normal),
+    Font(R.font.notosans_display_regular, FontWeight.W300),
+    Font(R.font.notosans_medium, FontWeight.Medium),
+    Font(R.font.notosans_bold, FontWeight.Bold),
+)
+
+private val InterFontFamily = FontFamily(
+    Font(R.font.inter_regular, FontWeight.Normal),
+)
+
 val Typography =
     Typography(
         bodyLarge =
@@ -37,6 +50,41 @@ val Typography =
 
 val Notosans =
     Typography(
+        headlineMedium = TextStyle(
+            fontFamily = NotoSansFontFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 0.sp,
+        ),
+        titleMedium = TextStyle(
+            fontFamily = NotoSansFontFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 20.sp,
+            lineHeight = 26.sp,
+            letterSpacing = 0.sp,
+        ),
+        displayMedium = TextStyle(
+            fontFamily = NotoSansFontFamily,
+            fontWeight = FontWeight.W300,
+            fontSize = 15.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.sp,
+        ),
+        bodySmall = TextStyle(
+            fontFamily = InterFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 15.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.sp,
+        ),
+        labelSmall = TextStyle(
+            fontFamily = NotoSansFontFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 14.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.sp,
+        ),
         bodyLarge =
             TextStyle(
                 fontFamily = FontFamily.Default,
@@ -51,12 +99,5 @@ val Notosans =
             fontSize = 22.sp,
             lineHeight = 28.sp,
             letterSpacing = 0.sp,
-        ),
-        labelSmall = TextStyle(
-            fontFamily = FontFamily.Default,
-            fontWeight = FontWeight.Medium,
-            fontSize = 11.sp,
-            lineHeight = 16.sp,
-            letterSpacing = 0.5.sp,
         ),
     )
