e1fe44410ca3f50e3290000c8099f3cd87474a39
Hogu59 <dbstjdguswkd@naver.com>
2025-02-21 11:32:03 +0900

    :sparkles: implement screen root and fragment

---
+++ android/app/src/main/java/net/pengcook/android/presentation/otherprofile/OtherProfileFragment.kt
@@ -0,0 +1,43 @@
+package net.pengcook.android.presentation.otherprofile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class OtherProfileFragment : Fragment() {
+    private val args: OtherProfileFragmentArgs by navArgs()
+    private val userId: Long by lazy { args.userId }
+
+    @Inject
+    lateinit var viewModelFactory: OtherProfileViewModelFactory
+
+    private val viewModel: OtherProfileViewModel by viewModels {
+        OtherProfileViewModel.provideFactory(viewModelFactory, userId)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View =
+        ComposeView(requireContext()).apply {
+            setContent {
+                OtherProfileScreenRoot(
+                    viewModel = viewModel,
+                    navigateBack = { navigateBack() },
+                )
+            }
+        }
+
+    private fun navigateBack() {
+        requireActivity().onBackPressedDispatcher.onBackPressed()
+    }
+}
