dafbbde926e398559bb949e35b43b6eaeab3d3ce
Hogu59 <dbstjdguswkd@naver.com>
2025-03-22 15:18:00 +0900

    :sparkles: implement presentation layer logic for Follow Screen

---
+++ android/app/src/main/java/net/pengcook/android/presentation/follow2/FollowListState.kt
@@ -0,0 +1,18 @@
+package net.pengcook.android.presentation.follow2
+
+import net.pengcook.android.presentation.follow2.model.FollowInfo
+
+data class FollowListState(
+    val userId: Long = 0L,
+    val username: String = "",
+    val selectedTabIndex: Int = 0,
+    val searchQuery: String = "",
+    val followers: List<FollowInfo> = emptyList(),
+    val followerCount: Long = 0L,
+    val followings: List<FollowInfo> = emptyList(),
+    val followingCount: Long = 0L,
+    val isLoading: Boolean = true,
+    val isMine: Boolean = false,
+    val showDialog: Boolean = false,
+    val selectedFollowInfo: FollowInfo? = null
+)
