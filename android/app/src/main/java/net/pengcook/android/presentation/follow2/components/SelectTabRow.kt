f0e00f0cd45bb67ddcb617d3041224b3766e266b
Hogu59 <dbstjdguswkd@naver.com>
2025-04-10 12:25:41 +0900

    :art: apply ktlint

--- android/app/src/main/java/net/pengcook/android/presentation/follow2/components/SelectTabRow.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/follow2/components/SelectTabRow.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.pengcook.android.presentation.follow2.FollowListAction
