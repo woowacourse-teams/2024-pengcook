588a7a66d8c99b4a589d6091b81be7e318d0c5ed
Hogu59 <dbstjdguswkd@naver.com>
2025-04-15 19:13:03 +0900

    :lipstick: add FollowListScreen preview and change FollowActionButton layout

--- android/app/src/main/java/net/pengcook/android/presentation/follow2/components/FollowActionButton.kt
+++ android/app/src/main/java/net/pengcook/android/presentation/follow2/components/FollowActionButton.kt
@@ -1,11 +1,13 @@
 package net.pengcook.android.presentation.follow2.components
 
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -20,11 +22,13 @@ fun FollowActionButton(
     onButtonClick: (Long) -> Unit,
     follower: FollowInfo,
     isFollowerInfo: Boolean,
+    modifier: Modifier = Modifier,
 ) {
     Button(
         onClick = { onButtonClick(follower.userId) },
         shape = RoundedCornerShape(8.dp),
         colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.surface),
+        modifier = modifier.height(40.dp),
     ) {
         Text(
             text = if (isFollowerInfo) {
