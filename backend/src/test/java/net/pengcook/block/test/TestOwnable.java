7d11155be032b5ec228ca6d520a885cd6b99abf0
haiseong <wjdgotjd9908@gmail.com>
2025-04-27 02:06:49 +0900

    :sparkles: OwnableFilteringHandler

---
+++ backend/src/test/java/net/pengcook/block/test/TestOwnable.java
@@ -0,0 +1,45 @@
+package net.pengcook.block.test;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import java.util.Objects;
+import net.pengcook.block.domain.Ownable;
+
+@Entity
+public class TestOwnable implements Ownable {
+
+    @Id
+    @GeneratedValue
+    private Long ownerId;
+
+    protected TestOwnable() {
+    }
+
+    public TestOwnable(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+
+    @Override
+    public long getOwnerId() {
+        return ownerId;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        TestOwnable ownable = (TestOwnable) object;
+        return Objects.equals(ownerId, ownable.ownerId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ownerId);
+    }
+}
